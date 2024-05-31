727dc002dbdea2c55a92b6b2963e5dc188dab23b
WasayWithCode <abdulwasay43429@gmail.com>
2024-05-31 14:44:39 +0500

    initial commit

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,9 +1,10 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import User from "../models/user.model";
-import { handleError } from "../../utils";
-import { connectToDatabase } from "../mongoose";
+
+import User from "../database/models/user.model";
+import { connectToDatabase } from "../database/mongoose";
+import { handleError } from "../utils";
 
 // CREATE
 export async function createUser(user: CreateUserParams) {
