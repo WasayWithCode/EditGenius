c8c0becdf93d9f5c86c1e1e48b9c3e7798df4051
WasayWithCode <abdulwasay43429@gmail.com>
2024-05-31 11:04:39 +0500

    initial commit

---
+++ lib/formUrlQuery.ts
@@ -0,0 +1,15 @@
+import qs from "qs";
+
+// ==== End
+// FORM URL QUERY
+export const formUrlQuery = ({
+  searchParams,
+  key,
+  value,
+}: FormUrlQueryParams) => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+
+  return `${window.location.pathname}?${qs.stringify(params, {
+    skipNulls: true,
+  })}`;
+};
